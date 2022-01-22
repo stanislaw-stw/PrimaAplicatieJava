c865b79237c040d1c24c91a6b59bbdd18ffb7292
andre <andreist1999@yahoo.com>
2022-01-22 16:53:19 +0200

    cod

---
+++ src/ro/java/ctrln/Main.java
@@ -0,0 +1,8 @@
+package ro.java.ctrln;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+    }
+}
