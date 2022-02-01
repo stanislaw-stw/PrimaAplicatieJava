207267adbc1e3fd5f7ff03e5f8c7c4866797bb8a
andre <andreist1999@yahoo.com>
2022-02-01 12:32:24 +0200

    ADAUGARE COMENTARII PE O SINGURA LINIE

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -2,7 +2,9 @@ package ro.java.ctrln;
 
 public class Main {
 
+    // ACEASTA ESTE METODA MAIN , CARE PORNESTE O APLICATIE JAVA
     public static void main(String[] args) {
+	   // Aici vreau sa afisez pe consola un mesaj ales de noi
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
